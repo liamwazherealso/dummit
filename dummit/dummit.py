586c0572a25083a40248314fd53a5f421a0ffdb9
Liam Pieri <liamwazherealso@gmail.com>
2021-01-17 20:04:52 -0500

    Add strand lookup

--- dummit/dummit.py
+++ dummit/dummit.py
@@ -5,14 +5,14 @@ import os
 @click.group()
 def main():
     global __DBYAML__
-    global strands
+    global strands_db
     __DBYAML__ = "strands.yml"
     with open(__DBYAML__, 'rb') as f:
-        strands = yaml.load(f, Loader=yaml.FullLoader)
+        strands_db = yaml.load(f, Loader=yaml.FullLoader)
 
 @click.command()
 @click.argument('conf', required=True, type=click.File('rb'))
-@click.argument('dockerfile', required=False, type=click.File('w'))
+@click.argument('dockerfile', required=False, type=click.File('w', encoding="ascii", errors="surrogateescape"))
 @click.option('--dry-run', is_flag=True, help="Print dockerfile")
 def generate(conf, dockerfile, dry_run):
     conf = yaml.load(conf, Loader=yaml.FullLoader)
@@ -30,19 +30,25 @@ def generate(conf, dockerfile, dry_run):
             raise MalformedConf
         strands[strand] = val
 
-    if "base" in strands:
-        baseImage = strands["base"]
-        del strands["base"]
+    if "base" in conf:
+        baseImage = conf["base"]
+        del conf["base"]
     
     # Catch special cases
     if "pytorch" in strands and "cuda" in strands:
-        baseImage = "pytorch:pytorch:{}-cuda{}-cudnn7-devel".format(strands["pytorch"], strands["cuda"])
+        baseImage = "pytorch/pytorch:{}-cuda{}-cudnn7-devel".format(strands["pytorch"], strands["cuda"])
         del strands["pytorch"]
         del strands["cuda"]
+
     contents += "ARG BASE_IMAGE={}\n".format(baseImage)
     contents += "FROM $BASE_IMAGE\n"
 
-    
+    # Add remaining strands
+    for strand, value in strands.items():
+        if value == -1:
+            contents += strands_db[strand]
+        else:
+            contents += strands_db[strand][value]
 
     if dry_run:
         print(contents)
