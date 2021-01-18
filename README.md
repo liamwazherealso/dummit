945d3944e1b9821788ed3d07a2cf5dc2f4a90f62
Liam Pieri <liamwazherealso@gmail.com>
2021-01-17 19:28:02 -0500

    Initial functionality and example with base

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Users of ML/DS docker, especially when cuda is involved, feel the pain of mixing
 
 This app takes in a simple YAML file list and concatenates the listed requirements to create a docker file.
 
-The goal of this project is to create simple dockerfiles from a small range of well used projects. It is not scoped to complete the larger (and ludicrous goal) of a "dockerfile creator from python dependencies". 
+The goal of this project is to create simple dockerfiles from a small range of commonly used projects/docker images. It is not scoped to complete the larger (and ludicrous goal) of a "dockerfile creator from python dependencies". 
 ## Setup
 ```sh
 # Install dependencies
@@ -16,7 +16,28 @@ pipenv run pre-commit install -t pre-commit
 pipenv run pre-commit install -t pre-push
 ```
 
+## Usage
+
+
+Example conf.yml
+
+```yaml
+---
+- "base==pytorch/pytorch:1.6.0-cuda10.1-cudnn7-devel"
+```
+
+Run
+```python
+python dummit.py generate conf.yaml Dockerfile
+```
+
+Produces Dockerfile
+```
+ARG BASE_IMAGE=pytorch/pytorch:1.6.0-cuda10.1-cudnn7-devel
+FROM $BASE_IMAGE
+```
 ## Credits
+
 This package was created with Cookiecutter and the [sourcery-ai/python-best-practices-cookiecutter](https://github.com/sourcery-ai/python-best-practices-cookiecutter) project template.
 
 Named after David S. Dummit. 
--- dummit/dummit.py
+++ dummit/dummit.py
@@ -1,5 +1,47 @@
-def fib(n: int) -> int:
-    if n < 2:
-        return n
+import click
+import yaml
+import os
+
+@click.group()
+def main():
+    global __DBYAML__
+    global strands
+    __DBYAML__ = "strands.yml"
+    with open(__DBYAML__, 'rb') as f:
+        strands = yaml.load(f, Loader=yaml.FullLoader)
+
+@click.command()
+@click.argument('conf', required=True, type=click.File('rb'))
+@click.argument('dockerfile', required=False, type=click.File('w'))
+@click.option('--dry-run', is_flag=True, help="Print dockerfile")
+def generate(conf, dockerfile, dry_run):
+    conf = yaml.load(conf, Loader=yaml.FullLoader)
+    contents = ""
+    strands = {}
+
+    # load conf
+    for strand in conf:
+        strand = strand.split("==")
+        if len(strand) == 1:
+            strand, val = strand[0], -1
+        elif len(strand) == 2:
+            strand, val = strand[0], strand[1]
+        else:
+            raise MalformedConf
+        strands[strand] = val
+
+    if "base" in strands:
+        contents += "ARG BASE_IMAGE={}\n".format(strands["base"])
+        contents += "FROM $BASE_IMAGE\n" 
+    if dry_run:
+        print(contents)
     else:
-        return fib(n - 1) + fib(n - 2)
+        dockerfile.write(contents)
+            
+class MalformedConf(Exception):
+    """Conf list item was not of the form `Dependency==Version`"""
+    pass
+
+main.add_command(generate)
+if __name__ == "__main__":
+    main()
